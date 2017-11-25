003587014e52026e8bb03529c1f4c21ba75187e2
Miha Zidar <zidarsk8@gmail.com>
2017-11-25 18:22:34 +0100

    Add example test for base app

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+# conftest.py
+import pytest
+
+from bajtahack import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.debug = True
+    app.testing = True
+    app.config["SERVER_NAME"] = "localhost"
+    return app
