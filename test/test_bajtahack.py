a1f3d9064e4a4950f5e9b52ae439ac94c23b1b8b
Miha Zidar <zidarsk8@gmail.com>
2017-11-25 23:13:01 +0100

    Add data verification to index tests

--- test/test_bajtahack.py
+++ test/test_bajtahack.py
@@ -6,3 +6,4 @@ def test_ping(app):
     with app.app_context():
         response = app.test_client().get(url_for('index'))
         assert response.status_code == 200
+        assert response.data == b'Hello World!'
