c2b790add7667c4ad85156815086394acdfd5d93
Miha Zidar <zidarsk8@gmail.com>
2017-11-25 21:57:00 +0100

    Add codecov to test requirements

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
                 'pylint',
             ],
             'test': [
+                'codecov',
                 'pytest-runner',
                 'pytest-flask',
                 'pytest',
